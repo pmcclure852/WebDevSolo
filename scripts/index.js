7c1ee7114cac2c1e1d37fc8f9f5b9ef110253b72
Kelly Barber <kellybarber@Kellys-MacBook-Pro.local>
2021-03-24 16:00:09 +0000

    Really getting going with meets page. Using a single js file for now and checking URL.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 
+console.log(document.URL);
+
 // All meet data, in ascending date order with dates in ISO 8601 format: YYYY-MM-DD
 
 var allData = [
@@ -18,54 +20,88 @@ var allData = [
         date: "2021-07-07", time: "18:00 - 22:00"}
 ];
 
-var sidebarData = [];
-
 // Create arrays to display human friendly days and months
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-// Loop the data and create array of the next 4 meets
-// TODO: What if allData if not in date order!?
+// Code for meet items on meets page
+if (document.URL.includes("meets.html")){
+    console.log("we're on meets page!")
 
-for (var i=0; i<allData.length; i++)
-{
-    if(Date.parse(allData[i].date) > Date.now()) {
+    // Check some check boxes!
+    if (document.getElementById("sport").checked){
+        console.log("Sport is checked!")
+    }
 
-        var dateParsed = new Date(allData[i].date);
-        allData[i].date = days[dateParsed.getDay()] + " " + dateParsed.getDate() + " " + months[dateParsed.getMonth()]
-            + " " + dateParsed.getFullYear();
-        sidebarData.push(allData[i]);
 
-        if (sidebarData.length >= 3) {
-            break;
+    // Create date object of now to use to compare months etc
+    var now = new Date(Date.now());
+
+    //Set month to this month first time round
+    var month = now.getMonth();
+
+    // Loop the data and create array of the selected month
+    for (var i = 0; i < allData.length; i++) {
+        var meetDate = new Date(Date.parse(allData[i].date));
+        if (meetDate.getMonth() === month) {
+            console.log("Lift-off!!!");
+            // var dateParsed = Date.parse(allData[i].date);
+            // allData[i].date = days[dateParsed.getDay()] + " " + dateParsed.getDate() + " " + months[dateParsed.getMonth()]
+            //     + " " + dateParsed.getFullYear();
+            // sidebarData.push(allData[i]);
+            //
+            // if (sidebarData.length >= 3) {
+            //     break;
+            // }
         }
     }
+
 }
 
+
+// Code for sidebar on index.html. only run if on home page!
+if (document.URL.includes("index.html")) {
+
+    var sidebarData = [];
+// Loop the data and create array of the next 4 meets
+// TODO: What if allData if not in date order!?
+    for (var i = 0; i < allData.length; i++) {
+        if (Date.parse(allData[i].date) > Date.now()) {
+
+            var dateParsed = new Date(allData[i].date);
+            allData[i].date = days[dateParsed.getDay()] + " " + dateParsed.getDate() + " " + months[dateParsed.getMonth()]
+                + " " + dateParsed.getFullYear();
+            sidebarData.push(allData[i]);
+
+            if (sidebarData.length >= 3) {
+                break;
+            }
+        }
+    }
+
 // Add array to object for the sidebar template
-var data = {listItems: sidebarData}
+    var data = {listItems: sidebarData}
 
 // Create and render sidebar template, looping through data object
 // Escape data to prevent code input causing errors and return safe string
 // TODO: links to meet items on meets page?
 
-var template2 = document.getElementById("sideBarTemplate2").innerHTML;
-var compiledTemp2 = Handlebars.compile(template2);
+    var template2 = document.getElementById("sideBarTemplate2").innerHTML;
+    var compiledTemp2 = Handlebars.compile(template2);
 
-Handlebars.registerHelper("meetingsList", function (listItems){
-    var output = "";
-    for (var i=0; i<listItems.length; i++)
-    {
-        output +=   "<div>" +
-                        "<h3>" + Handlebars.Utils.escapeExpression(listItems[i].header) + "</h3>" +
-                        "<p>" + listItems[i].date + "<br>" + listItems[i].time + "</p>" +
-                        "<p>" + Handlebars.Utils.escapeExpression(listItems[i].details) + "</p>" +
-                    "</div>"
-    }
-    return new Handlebars.SafeString(output);
-});
+    Handlebars.registerHelper("meetingsList", function (listItems) {
+        var output = "";
+        for (var i = 0; i < listItems.length; i++) {
+            output += "<div>" +
+                "<h3>" + Handlebars.Utils.escapeExpression(listItems[i].header) + "</h3>" +
+                "<p>" + listItems[i].date + "<br>" + listItems[i].time + "</p>" +
+                "<p>" + Handlebars.Utils.escapeExpression(listItems[i].details) + "</p>" +
+                "</div>"
+        }
+        return new Handlebars.SafeString(output);
+    });
 
-var renderedTemp2 = compiledTemp2(data);
-document.getElementById("template2Target").innerHTML = renderedTemp2;
-
+    var renderedTemp2 = compiledTemp2(data);
+    document.getElementById("template2Target").innerHTML = renderedTemp2;
+}
 
